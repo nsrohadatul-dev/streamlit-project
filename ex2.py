76db09cd0c724178762cf253a6f48b24de5e6d37
nsrohadatul-dev <nsrohadatul@gmail.com>
2026-09-18 14:39:50 +0800

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("Ticket Price")
+
+price = st.text_input("Enter ticket price:")
+
+if st.button("Submit"):
+    try:
+        price = float(price)
+    except:
+        st.write("Invalid Price")
+    else:
+        st.write("Ticket Price:", price)
